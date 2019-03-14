56c34fa2b126de876448255a430a34fbcdea6e62
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-03-14 12:09:18 -0700

    Serve root page

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.send('YaMovie Server');
+});
+
 app.use('/api', apiRouter);
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
