09124c112de0ffd268199b8894bd650b56ce828a
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-03-14 12:01:50 -0700

    Remove docs

--- index.js
+++ index.js
@@ -18,13 +18,6 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  fs.readFile(path.join(__dirname, 'docs', 'docs.md'), 'utf8', (err, data) => {
-    if (err) console.log(err);
-    res.send(marked(data.toString()));
-  });
-});
-
 app.use('/api', apiRouter);
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,22 @@ const router = express.Router();
 const tmdb = require('./tmdb');
 const db = require('../data/db');
 
+/**
+ * NOTES:
+ *
+ * All requests will be primarily served data from local database.
+ *
+ * If data is not found, data will be queried via TMDB API, stored locally,
+ * then served to request.
+ *
+ * All data will be formatted in JSON.
+ */
+
+/**
+ * Returns all data from every movie.
+ *
+ * @return  data of every movie
+ */
 router.get('/movies', (req, res) => {
   if (
     !db.has('movies').value() ||
@@ -20,6 +36,12 @@ router.get('/movies', (req, res) => {
   return res.json(db.get('movies').value());
 });
 
+/**
+ * Returns all data of a specific movie
+ *
+ * @param   id  the movie id, found in request
+ * @return      movie data
+ */
 router.get('/movies/:id', (req, res) => {
   const movie = db
     .get('movies')
@@ -32,6 +54,9 @@ router.get('/movies/:id', (req, res) => {
     : res.status(404).json({ Error: 'Bad request: Invalid ID' });
 });
 
+/**
+ * Returns an array of movie genres
+ */
 router.get('/genres', (req, res) => {
   if (
     !db.has('genres').value() ||
