c738e91891fbc1aede46965a930dbed6aa71e097
Ivy <yunjilim@gmail.com>
2019-03-29 08:17:31 -0700

    adding oauth setup

---
+++ routes/oauth.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const router = express.Router();
+const passport = require('passport');
+const oauthController = require('../controllers/oauthController');
+ 
+const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] });
+
+// This custom middleware allows us to attach the socket id to the session.
+// With the socket id attached we can send back the right user info to the right socket
+const addSocketIdtoSession = (req, res, next) => {
+  req.session.socketId = req.query.socketId;
+  next();
+};
+
+// Routes that are triggered by the React client
+router.get('/google', addSocketIdtoSession, googleAuth);
+
+// Routes that are triggered by callbacks from OAuth providers once 
+// the user has authenticated successfully
+router.get('/google/callback', googleAuth, oauthController.google);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
+
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const socketio = require('socket.io');
 const logger = require('./middleware/logger');
 const apiRoutes = require('./routes/api');
+const oauthRoutes = require('./routes/api');
 const seed = require('./utils/seed');
+
+require('./config/passport');
 
 const app = express();
 
@@ -18,11 +24,26 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(session({
+  secret: 'Hello there!',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connecting sockets to the server and adding them to the request 
+// so that we can access them later in the controller
+const io = socketio(app);
+app.set('io', io);
+
 app.get('/', (req, res) => {
   res.send('YaMovie Server');
 });
 
 app.use('/api', apiRoutes);
+app.use('/auth', oauthRoutes);
 
 process.on('unhandledRejection', (error, promise) => {
   console.log('Unhandled Rejection at:', error.stack || error);
