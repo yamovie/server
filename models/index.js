39c14d87abae85d813d993e4c9ee823253f4e8d4
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-03-30 20:21:49 -0700

    Improve mongoose connection logging

--- models/index.js
+++ models/index.js
@@ -15,12 +15,18 @@ const options = {
 mongoose.set('debug', true);
 mongoose.set('objectIdGetter', true);
 
-mongoose
-  .connect(
-    `${process.env.DB_SCHEME}${process.env.DB_HOST}${process.env.DB_NAME}`,
-    options,
-  )
-  .then(() => console.log('Database connected.'));
+mongoose.connect(
+  `${process.env.DB_SCHEME}${process.env.DB_HOST}${process.env.DB_NAME}`,
+  options,
+);
+
+mongoose.connection.on('connected', () =>
+  console.log(`Connected to ${process.env.DB_NAME}`),
+);
+
+mongoose.connection.on('disconnected', () =>
+  console.log(`Disconnected from ${process.env.DB_NAME}`),
+);
 
 module.exports.Movie = require('./movie');
 module.exports.Genre = require('./genre');
