ce309c361c0622b065f3caecadcd9a65b86c6661
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-04-30 12:49:22 -0700

    Rename files to match conventions

--- controllers/index.js
+++ controllers/index.js
@@ -5,3 +5,4 @@ module.exports.genre = require('./genre');
 module.exports.movie = require('./movie');
 module.exports.provider = require('./provider');
 module.exports.user = require('./user');
+module.exports.preference = require('./preference');
