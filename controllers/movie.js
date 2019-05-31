d027da4c8f4fe96b13ee110583b1d02674b4404d
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-05-31 09:24:31 -0700

    updated based on requested changes

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,5 +1,5 @@
 const { Movie } = require('../models');
-const { recommendations, requests } = require('../configs');
+const { requests } = require('../configs');
 
 const readBySearch = async (req, res) => {
   const regex = new RegExp(req.query.searchInput, 'gi');
@@ -53,8 +53,8 @@ const readByGenre = async (req, res) => {
 };
 
 const readByRecommendation = async (req, res) => {
-  console.log(req.body);
-  const { certifications, genres, release, ratings, providers, foreign } = req.body;
+  // console.log(req.body);
+  const { certifications, genres, release, ratings, foreign } = req.body;
 
   // const applicableCerts = recommendations.getCertifications(req.body.certification);
   const applicableCerts = Object.keys(certifications).filter(
@@ -73,7 +73,9 @@ const readByRecommendation = async (req, res) => {
     'ratings.imdb.value': { $gte: ratings.imdb.minRating, $lte: ratings.imdb.maxRating },
   };
   if (!foreign) conditions.original_language = 'en';
-  // TODO: add provider checking?
+  // TODO: Potentially add provider checking/filtering. Currently no good way to do so
+  // based on how the database is structured, so needs to be handled on front end for now
+  // const { providers } = req.body;
   // if (providers) {
   //   const applicableProviders = Object.keys(providers).filter(provKey => providers[provKey]);
   //   conditions['offers.stream[??].provider._id'] = { $in: applicableProviders };
